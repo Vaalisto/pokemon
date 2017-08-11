696d967e7a8a1b7e03dc86e51c527693a0db5858
Ville Vaalisto <ville.vaalisto@gmail.com>
2017-08-11 23:02:23 +0300

    JSON now shows keys correctly

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = make_dicts
     return db
 
 def query_db(query, args=(), one=False):
@@ -16,6 +17,10 @@ def query_db(query, args=(), one=False):
     rv = cur.fetchall()
     cur.close()
     return (rv[0] if rv else None) if one else rv
+
+def make_dicts(cursor, row):
+    return dict((cursor.description[idx][0], value)
+                for idx, value in enumerate(row))
 
 @app.teardown_appcontext
 def close_connection(exception):
@@ -29,7 +34,7 @@ def hello_world():
 
 @app.route('/pokemons', methods = ['GET'])
 def pokemons():
-    rows = query_db("select * from pokemon",[],one=False)
+    rows = query_db("select name, number, type1, type2 from pokemon",[],one=False)
     return jsonify({'pokemons':rows})
 
 if __name__ == '__main__':
